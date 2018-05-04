4fc5796479df33f871a44a7beed9885455de03d6
tonyfast <tony.fast@gmail.com>
2018-05-04 08:08:26 -0400

    Instructions for using IPython

--- src/importnb/__init__.py
+++ src/importnb/__init__.py
@@ -1,8 +1,4 @@
 __all__ = 'Notebook', 'Partial', 'reload', 'load_ipython_extension', 'unload_ipython_extension'
 
 from .loader import Notebook, Partial, load_ipython_extension, unload_ipython_extension, reload
-
-try:
-    import IPython
-    from .utils.ipython import install, uninstall
-except: ...
+from . import utils
