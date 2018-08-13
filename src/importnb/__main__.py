70796cdaa0ccd01c5b40bae02be3f32f1d47a604
Tony Fast <tony.fast@gmail.com>
2018-08-13 09:38:14 -0400

    Add the main condition

--- src/importnb/__main__.py
+++ src/importnb/__main__.py
@@ -2,6 +2,7 @@ from .parameterize import Parameterize
 import sys
 file = sys.argv[1] if len(sys.argv) > 1 else None
 
-sys.argv = [sys.argv[0], *sys.argv[2:]]
+if file == __file__:
+    sys.argv = [sys.argv[0], *sys.argv[2:]]
 
-file and Parameterize.load(file)
+    file and Parameterize.load(file)
