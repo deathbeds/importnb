caf27e1d367715a54d456aea58acbd8c4882675b
Tony Fast <tony.fast@gmail.com>
2018-08-13 09:44:46 -0400

    no star assignment

--- src/importnb/__main__.py
+++ src/importnb/__main__.py
@@ -3,6 +3,6 @@ import sys
 file = sys.argv[1] if len(sys.argv) > 1 else None
 
 if file == __file__:
-    sys.argv = [sys.argv[0], *sys.argv[2:]]
+    sys.argv = [sys.argv[0]] + [sys.argv[2:]]
 
     file and Parameterize.load(file)
