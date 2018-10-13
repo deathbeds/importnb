801e2450973dae9d89a4ac50f0fe27913f2ce14a
Tony Fast <tony.fast@gmail.com>
2018-10-13 15:03:53 -0400

    Use zip safe

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ setup_args = dict(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3 :: Only',],
-    zip_safe=False,
+    zip_safe=True,
     cmdclass={'test': PyTest,},
     entry_points = {
         'pytest11': ['pytest-importnb = importnb.utils.pytest_plugin',],
