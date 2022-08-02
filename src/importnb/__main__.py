2b384736a810680bc4aa3905157003c339778401
tonyfast <tony.fast@gmail.com>
2022-08-02 16:56:46 -0700

    comment the main methods

--- src/importnb/__main__.py
+++ src/importnb/__main__.py
@@ -1,27 +1,30 @@
-from argparse import ArgumentParser
 from pathlib import Path
 import runpy
 from shlex import split
 import sys
 
 
-PARSER = ArgumentParser(
-    prog="importnb", usage="a command line interface for executing notebooks as scripts."
-)
-COMMANDS = PARSER.add_subparsers(dest="f")
-COMMANDS.add_parser(
-    "install",
-    help="install the importnb extension",
-)
-COMMANDS.add_parser("uninstall", help="uninstall the importnb extension")
-RUN = COMMANDS.add_parser("run", help="run files and modules as scripts")
-RUN.add_argument("file", nargs="*")
-RUN.add_argument("-m", "--module", nargs="*")
+def make_parser():
+    from argparse import ArgumentParser
 
+    PARSER = ArgumentParser(
+        prog="importnb", usage="a command line interface for executing notebooks as scripts."
+    )
+    COMMANDS = PARSER.add_subparsers(dest="f")
+    COMMANDS.add_parser(
+        "install",
+        help="install the importnb extension",
+    )
+    COMMANDS.add_parser("uninstall", help="uninstall the importnb extension")
+    RUN = COMMANDS.add_parser("run", help="run files and modules as scripts")
+    RUN.add_argument("file", nargs="*")
+    RUN.add_argument("-m", "--module", nargs="*")
+    return PARSER
 
 def run(ns):
     from importnb import Notebook
 
+    # add our current directory to the system path
     sys.path.insert(0, str(Path().absolute()))
 
     # run files requested from the cli
@@ -35,6 +38,7 @@ def run(ns):
 
 
 def main(argv=None):
+    """a convenience function for running importnb as an application"""
     from .utils.ipython import install, uninstall
 
     argv = argv or sys.argv[1:]
@@ -42,23 +46,25 @@ def main(argv=None):
     if isinstance(argv, str):
         argv = split(argv)
 
+    PARSER = make_parser()
     if not argv:
+        # show help when no arguments are supplied
         return PARSER.print_help()
     if argv[0] not in {"install", "uninstall", "run"}:
+        # if no command is supplied then we default to the run command
         argv.insert(0, "run")
+
     ns = PARSER.parse_args(argv)
 
     ns.f = ns.f or "run"
 
     if ns.f == "run":
+        # execute the run command
         return run(ns)
 
+    # dispatch the ipython extension installations
     return dict(install=install, uninstall=uninstall)[ns.f]()
 
-
-from .parameterize import Parameterize
-
-file = sys.argv[1] if len(sys.argv) > 1 else None
 
 if __name__ == "__main__":
     main()
