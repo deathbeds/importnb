2c35d1b5fa95c1c16908ca32c47b7d82202e9a59
tonyfast <tony.fast@gmail.com>
2022-08-03 22:10:35 -0700

    add tests for the new cli

--- noxfile.py
+++ noxfile.py
@@ -3,6 +3,7 @@ from os import environ
 
 CI = bool(environ.get("CI"))
 E = ("-e", "")[CI]
+PIP = tuple()
 
 
 @session(reuse_venv=True)
@@ -11,5 +12,7 @@ def test(session, env):
     e = [E + ".[test]"]
     if env == "ipy":
         e.extend(["nbconvert", "ipython"])
-    session.install(*e)
+    session.install(*e, *PIP)
     session.run("pytest", "tests", *session.posargs)
+    if not session.posargs:
+        session.run("pytest", "tests/test_cli.ipynb", "--nbval")
