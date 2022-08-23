d57969aabf8cfb8874cf065c34d7dfb73e0e445e
tonyfast <tony.fast@gmail.com>
2022-08-23 11:41:54 -0700

    update the doit task

--- dodo.py
+++ dodo.py
@@ -1,7 +1,7 @@
-def task_gen_parser():
-    """generate the standlone pidgy json parser"""
+def task_build():
     return dict(
-        actions=["python -m lark.tools.standalone --propagate_positions src/nb.g > src/importnb/_json_parser.py"],
-        file_dep=["dodo.py", "src/nb.g"],
-        targets=["src/importnb/_json_parser.py"]
+        actions=["hatch build"],
+        file_dep=["pyproject.toml", "src/nb.g"],
+        targets=["src/importnb/_json_parser.py", "src/importnb/_version.py"],
+        clean=True,
     )
