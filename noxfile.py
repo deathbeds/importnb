f84819cb0678a8286fe2524dcc7179ea39318d44
tonyfast <tony.fast@gmail.com>
2022-08-23 21:16:28 -0700

    update workflow

--- noxfile.py
+++ noxfile.py
@@ -1,13 +1,19 @@
+from platform import python_implementation
 from nox import session, parametrize
 from os import environ
 
 CI = bool(environ.get("CI"))
 E = ("-e", "")[CI]
 PIP = tuple()
+PYPY = python_implementation() == "PyPy"
 
+sessions = ["py"]
+if not PYPY:
+    sessions += ["ipy"]
 
+if 
 @session(reuse_venv=True)
-@parametrize("env", ["py", "ipy"])
+@parametrize("env", sessions)
 def test(session, env):
     e = [E + ".[test]"]
     if env == "ipy":
