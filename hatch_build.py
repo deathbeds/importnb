a280de1383c1404a60448729e8e3f2aa486e7250
Tony Fast <tony.fast@gmail.com>
2022-08-23 21:29:41 -0700

    use hatchling as the build backend (#129)
    * remove pytest warnings and nbdoctest idea
    * remove setuptools and lark grammar.
    * capitalize readme
    * use hatch as the build backend
    * add vcs config
    * Update pyproject.toml
    Co-authored-by: Ofek Lev <ofekmeister@gmail.com>
    * use python sys executable
    * mv lark build dependency to the custom hooks
    * add generated build files to the gitignore and hatch build
    * update the doit task
    * explicitly define the lark standalone parser as a build artifact`
    * configure license properly in pyproject
    * trim test workflow
    * Update hatch_build.py
    
    Co-authored-by: Ofek Lev <ofekmeister@gmail.com>
    
    * Update hatch_build.py
    
    Co-authored-by: Ofek Lev <ofekmeister@gmail.com>
    
    * simpify check call
    
    * rname grammar to json because thats all it really is
    
    * sp
    
    * use lark internals for generating the standalone grammar
    
    * Update hatch_build.py
    
    Co-authored-by: Ofek Lev <ofekmeister@gmail.com>
    
    * Update hatch_build.py
    
    Co-authored-by: Ofek Lev <ofekmeister@gmail.com>
    
    * update workflow
    
    * nox
    
    Co-authored-by: Ofek Lev <ofekmeister@gmail.com>

---
+++ hatch_build.py
@@ -0,0 +1,53 @@
+from functools import partial
+from io import StringIO
+from logging import shutdown
+from pathlib import Path
+import os
+import shlex
+from subprocess import call
+import sys
+from hatchling.builders.hooks.plugin.interface import BuildHookInterface
+
+
+class LarkStandAloneBuildHook(BuildHookInterface):
+    PLUGIN_NAME = "lark_standalone"
+
+    def initialize(self, version, build_data):
+        L = get_logger()
+        WIN = os.name == "nt"
+        L.info("converting json grammar to python")
+        python_parser = Path(self.root, "src/importnb/_json_parser.py")
+        if not python_parser.exists():
+            py = get_standalone()
+            python_parser.write_text(py)
+        build_data["artifacts"].append(
+            "/src/importnb/_json_parser.py"
+        )  # its really important to remember the preceeding /
+
+
+def get_logger():
+    import logging
+
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    logging.basicConfig(level=logging.INFO)
+    return logger
+
+
+def get_lark():
+    from lark.tools.standalone import lalr_argparser, build_lalr
+
+    return build_lalr(lalr_argparser.parse_args(["--propagate_positions", "src/json.g"]))[0]
+
+
+def write(buffer, *lines):
+    buffer.writelines(map(str, lines or ["\n"]))
+
+
+def get_standalone():
+    from lark.tools.standalone import gen_standalone
+
+    lark = get_lark()
+    python = StringIO()
+    gen_standalone(lark, partial(print, file=python))
+    return python.getvalue()
