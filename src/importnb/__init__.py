2b91ba636d44484a7a5e95be1c5ee774f600e918
tonyfast <tony.fast@gmail.com>
2022-09-21 22:14:17 -0700

    rm unnecessary import

--- src/importnb/__init__.py
+++ src/importnb/__init__.py
@@ -4,4 +4,4 @@ __all__ = "Notebook", "reload", "Parameterize"
 
 from ._version import *
 from .ipython_extension import load_ipython_extension, unload_ipython_extension
-from .loader import Notebook, reload, unload_ipython_extension
+from .loader import Notebook, reload
