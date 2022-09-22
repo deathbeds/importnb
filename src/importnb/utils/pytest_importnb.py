11a273eb24f2566e1a8743c5d3883719a01eaee0
tonyfast <tony.fast@gmail.com>
2022-09-21 22:14:07 -0700

    simplify pytest extension

--- src/importnb/utils/pytest_importnb.py
+++ src/importnb/utils/pytest_importnb.py
@@ -4,50 +4,32 @@
 The `AlternativeModule` is reusable.  See `pidgin` for an example.
 """
 
-import abc
-import functools
-import importlib
 from pathlib import Path
 
-import _pytest
 import pytest
 
 from importnb import Notebook
 
 
-def pytest_addoption(parser):
-    group = parser.getgroup("general")
-    group.addoption("--main", action="store_true", help="Run in the main context.")
-
-
 class AlternativeModule(pytest.Module):
     def _getobj(self):
-        return self.loader(
-            getattr(self.parent.config.option, "main", None) and "__main__" or self.path
-        ).load(str(self.path))
+        return self.loader.load_file(str(self.path), False)
+
+    @classmethod
+    def pytest_collect_file(cls, parent, path):
+        if not parent.session.isinitpath(path):
+            for pat in parent.config.getini("python_files"):
+                if path.fnmatch(pat.rstrip(".py") + path.ext):
+                    break
+            else:
+                return
+        if hasattr(cls, "from_parent"):
+            return cls.from_parent(parent, path=Path(path))
+        return cls(path, parent)
 
 
 class NotebookModule(AlternativeModule):
     loader = Notebook
 
 
-class AlternativeSourceText(abc.ABCMeta):
-    def __call__(self, parent, path):
-        for module in self.modules:
-            if "".join(Path(str(path)).suffixes) in module.loader.extensions:
-                if not parent.session.isinitpath(path):
-                    for pat in parent.config.getini("python_files"):
-                        if path.fnmatch(pat.rstrip(".py") + path.ext):
-                            break
-                    else:
-                        return
-                if hasattr(module, "from_parent"):
-                    return module.from_parent(parent, path=Path(path))
-                return module(path, parent)
-
-
-class NotebookTests(metaclass=AlternativeSourceText):
-    modules = (NotebookModule,)
-
-
-pytest_collect_file = NotebookTests.__call__
+pytest_collect_file = NotebookModule.pytest_collect_file
