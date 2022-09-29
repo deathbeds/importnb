4b2ed5c6450f939061f10a306c0d8a9a8e8c378e
tonyfast <tony.fast@gmail.com>
2022-09-28 20:52:14 -0700

    make a shim for get_ipython

--- src/importnb/__init__.py
+++ src/importnb/__init__.py
@@ -1,7 +1,24 @@
 # coding: utf-8
 
-__all__ = "Notebook", "reload", "Parameterize"
+__all__ = "Notebook", "reload"
+
+def is_ipython():
+    from sys import modules
+    
+    return "IPython" in modules
+    
+def get_ipython():
+    if is_ipython():
+        from IPython import get_ipython
+        
+        return get_ipython()
+    return None
 
 from ._version import *
 from .ipython_extension import load_ipython_extension, unload_ipython_extension
 from .loader import Notebook, reload
+
+import builtins
+builtins.true, builtins.false, builtins.null = True, False, None
+
+    
