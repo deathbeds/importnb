b3d4836d8b77ed5210a48507be0205c2492a79a0
tonyfast <tony.fast@gmail.com>
2022-09-29 08:58:28 -0700

    rm nox dep

--- conftest.py
+++ conftest.py
@@ -3,6 +3,6 @@ try:
 
     InteractiveShell.instance()
 except:
-    collect_ignore = ["src/importnb/utils/ipython.py", "src/importnb/completer.py", "noxfile.py"]
+    collect_ignore = ["src/importnb/utils/ipython.py", "src/importnb/completer.py"]
 
 pytest_plugins = ["pytester"]
