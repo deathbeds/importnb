478077cf167c5805b25d44e9bf27349ec9307f58
Nicholas Bollweg <nick.bollweg@gmail.com>
2024-10-29 19:23:43 -0500

    a brief lite excursion

---
+++ lite/docs_hooks.py
@@ -0,0 +1,20 @@
+import shutil
+from pathlib import Path
+
+from mkdocs.config.defaults import MkDocsConfig
+from mkdocs.structure.files import Files
+
+LITE = Path(__file__).parent.parent / "build/lite"
+
+
+def on_files(files: Files, config: MkDocsConfig) -> Files:
+    """Copy the schema to the site."""
+    lite_paths = sorted([p for p in LITE.rglob("*") if not p.is_dir()])
+    print(f"\t     - [lite] copying {len(lite_paths)} files")
+    for src in lite_paths:
+        dest = Path(config.site_dir) / f"lite/{src.relative_to(LITE).as_posix()}"
+        if dest.exists():
+            dest.unlink()
+        dest.parent.mkdir(parents=True, exist_ok=True)
+        shutil.copy2(src, dest)
+    return files
