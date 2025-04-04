dddd9a9c08836403d71c5c4b8058bafb937accca
Nicholas Bollweg <nick.bollweg@gmail.com>
2025-04-04 14:56:45 -0500

    look into ipython 9.x

--- src/importnb/__init__.py
+++ src/importnb/__init__.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-import os
+import sys
 from typing import TYPE_CHECKING
 
 __all__ = "Notebook", "__version__", "imports", "reload"
@@ -10,40 +10,19 @@ __all__ = "Notebook", "__version__", "imports", "reload"
 if TYPE_CHECKING:
     from IPython.core.interactiveshell import InteractiveShell
 
-_BEAR_ = len(os.environ.get("IMPORTNB_BEARTYPE", ""))
-
-if _BEAR_:
-    from beartype import BeartypeConf
-    from beartype.claw import beartype_all, beartype_this_package
-
-    beartype_this_package(
-        conf=BeartypeConf(
-            violation_type=UserWarning,
-            claw_skip_package_names=(
-                "importnb._json_parser",
-                "IPython.core.inputsplitter",
-            ),
-        )
-    )
-
-    if _BEAR_ > 2:
-        beartype_all(conf=BeartypeConf(violation_type=UserWarning))
-
 
 def is_ipython() -> bool:
-    from sys import modules
-
-    return "IPython" in modules
+    return "IPython" in sys.modules
 
 
 def get_ipython(force: bool | None = True) -> InteractiveShell | None:
     shell: InteractiveShell | None = None
     if force or is_ipython():
         try:
-            from IPython.core.getipython import get_ipython
+            from IPython.core import getipython
         except ModuleNotFoundError:
             return None
-        shell = get_ipython()  # type: ignore[no-untyped-call]
+        shell = getipython.get_ipython()  # type: ignore[no-untyped-call]
         if shell is None:
             from IPython.core.interactiveshell import InteractiveShell
 
