6ef7069b6d3169eac2ba496e9eb223e65418453a
Nicholas Bollweg <nick.bollweg@gmail.com>
2025-05-09 08:03:31 -0500

    bump lite, build conda packages

--- lite/mkdocs_hooks.py
+++ lite/mkdocs_hooks.py
@@ -4,15 +4,25 @@ from pathlib import Path
 from mkdocs.config.defaults import MkDocsConfig
 from mkdocs.structure.files import Files
 
-LITE = Path(__file__).parent.parent / "build/lite"
+ROOT = Path(__file__).parent.parent
+LITE = ROOT / "build/lite"
+DIST = ROOT / "dist"
+CONDA_DIST = DIST / "conda/noarch"
 
 
 def on_files(files: Files, config: MkDocsConfig) -> Files:
     """Copy the schema to the site."""
+    site_dir = Path(config.site_dir)
     lite_paths = sorted([p for p in LITE.rglob("*") if not p.is_dir()])
     print(f"\t     - [lite] copying {len(lite_paths)} files")
     for src in lite_paths:
-        dest = Path(config.site_dir) / f"lite/{src.relative_to(LITE).as_posix()}"
+        dest = site_dir / f"lite/{src.relative_to(LITE).as_posix()}"
+        if dest.exists():
+            dest.unlink()
+        dest.parent.mkdir(parents=True, exist_ok=True)
+        shutil.copy2(src, dest)
+    for src in [*CONDA_DIST.glob("*.conda"), *CONDA_DIST.glob("*.json")]:
+        dest = site_dir / f"conda/{src.relative_to(CONDA_DIST.parent).as_posix()}"
         if dest.exists():
             dest.unlink()
         dest.parent.mkdir(parents=True, exist_ok=True)
